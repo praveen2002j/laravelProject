fec54d167d856778369b2677b1fb904a4b630932
praveen2002j <jeyachandranpraveen2002@gmail.com>
2024-07-08 22:36:05 +0530

    firstcommit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+@section('title','Registration')
+@section('content')
+    <div class="container">
+    <form class="ms-auto me-auto mt-auto" style="width: 500px">
+ 
+  <div class="mb-3">
+    <label class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email">
+    
+  </div>
+  <div class="mb-3">
+    <label  class="form-label">Password</label>
+    <input type="password" class="form-control"name="password">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+    </div>
+
+ 
+
+
+@endsection
